565cfc03ce14a5e7c37b5cd45530be5df355b013
YuHyeongJoo <21700473@handong.edu>
2020-06-05 12:49:27 +0900

    f

--- src/problem1/problem1.c
+++ src/problem1/problem1.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/* TODO */
+#include <sys/types.h>
+#include<unistd.h>
 
 char * 
 read_exec (char * exe)
 {
-/* TODO*/
+	int fd = open("a.out",O_WRONLY | O_CREAT, 0644);
+	int dup2(fd,1);
+	char buffer[2048];
+	read(fd, buffer,2048);
+	return buffer;
 }
+
 
 int 
 main (int argc, char ** argv)
--- src/problem3/mysem.c
+++ src/problem3/mysem.c
@@ -1,24 +1,41 @@
-/*TODO*/
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+
 
 typedef struct {
-	//TODO
+	int sema;
+	cond_t cond;
+	mutex_t mu;
 } mysem_t ;
 
 
 void
 mysem_init (mysem_t * sem, int init)
 {
-	//TODO
+	Pthread_mutex_lock(&sem.mu);
+	sem.sema = init;
+	Pthread_mutex_unlock(&sem.mu);
 }
 
 void
 mysem_post (mysem_t * sem)
 {
-	//TODO
+	Pthread_mutex_lock(&sem.mu);
+	sem.sema++;
+	if(sem.sema==0)
+		Pthread_cond_signal(&sem.cond);
+	Pthread_mutex_unlock(&sem.mu);
+
 }
 
 void
 mysem_wait (mysem_t * sem)
 {
-	//TODO
+	Pthread_mutex_lock(&sem.mu);
+	sem.sema--;
+	if(sem.sema<0)
+		Pthread_cond_wait(&sem.cond, &sem.mu);
+	Pthread_mutex_unlock(&sem.mu);
+
 }
